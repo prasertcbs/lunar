2768124b4ed8ce0d6a00ffb646e11b267c424674
prasertcbs <bluecotton.k@gmail.com>
2020-03-26 14:42:30 +0700

    first commit

---
+++ demo_debug.py
@@ -0,0 +1,16 @@
+# To add a new cell, type '# %%'
+# To add a new markdown cell, type '# %% [markdown]'
+# %%
+def rectangle(w, h):
+    return w*h
+
+def triangle(w, h):
+    return .5 * rectangle(w, h)
+
+area=triangle(10, 4)
+print(area)
+
+
+# %%
+
+
